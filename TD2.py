585b4540e9b6b5287a8956a13684ce869bc4faf2
romain-adr <romain_audureau@hotmail.fr>
2022-11-03 15:52:36 +0100

    03/11/22

--- TD2.py
+++ TD2.py
@@ -8,6 +8,8 @@ configs = [[0,0,0,0], [0,0,0,1], [0,0,1,0], [0,0,1,1],
            [1,0,0,0], [1,0,0,1], [1,0,1,0], [1,0,1,1],
            [1,1,0,0], [1,1,0,1], [1,1,1,0], [1,1,1,1]]
 
+
+#fonction d'activation seuil
 f_activation = lambda x: 1 if x > 0 else 0
 
 # on part de la config "0" :
@@ -20,17 +22,26 @@ p1 = list(p[0])
 cellule=0
 
 #print(cellule)
-#print(p1)
+print("-------config-----")
+print(config)
 
 for cellule in range(4):
     a = []
     somme = 0
+    print("-----Cellule-----")
     print(cellule)
+    print("-----List--------")
     print(list(configs[cellule]))
     for i in range(4):
+        print("------config[i]----")
+        print(config[i])
+        print("------p[cellule][i]----")
+        print(p[cellule][i])
+        # somme de p + conf
         somme += p[cellule][i]+config[i]
+        #activation de la somme 1>0 sinon 0
         config[cellule] = f_activation(somme)
-        #print(config[cellule])
+        #ajouter dans list a=[]
         a.append(config[cellule])
-        
+    print("-----Sortie------")
     print(a)
