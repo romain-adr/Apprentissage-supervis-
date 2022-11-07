e2aeb73ca095e38068d80c4a4ab5e588975bc7f8
romain-adr <romain_audureau@hotmail.fr>
2022-11-07 11:42:59 +0100

    07/11/22 11:43 finir pour lundi pro

--- TD2.py
+++ TD2.py
@@ -14,34 +14,31 @@ f_activation = lambda x: 1 if x > 0 else 0
 
 # on part de la config "0" :
 
-#config = [0, 0, 0, 0]
-config = list(configs[0])
 
-# testons la cellule "a" aka "cellule 0" :
-p1 = list(p[0])
-cellule=0
+for c in range(16):
 
-#print(cellule)
-print("-------config-----")
-print(config)
+        #config = [0, 0, 0, 0]
+        config = list(configs[c])
 
-for cellule in range(4):
-    a = []
-    somme = 0
-    print("-----Cellule-----")
-    print(cellule)
-    print("-----List--------")
-    print(list(configs[cellule]))
-    for i in range(4):
-        print("------config[i]----")
-        print(config[i])
-        print("------p[cellule][i]----")
-        print(p[cellule][i])
-        # somme de p + conf
-        somme += p[cellule][i]+config[i]
-        #activation de la somme 1>0 sinon 0
-        config[cellule] = f_activation(somme)
-        #ajouter dans list a=[]
-        a.append(config[cellule])
-    print("-----Sortie------")
-    print(a)
+        # testons la cellule "a" aka "cellule 0" :
+        p1 = list(p[0])
+        cellule=0
+
+        #print(cellule)
+        print(config, end="")
+
+#faire une boucle jusqu'a etat stable:
+        for toto in range(4):
+
+             a = []
+             
+             for cellule in range(4):
+                
+                somme = 0
+                for i in range(4):
+                    somme += p[cellule][i]*config[i]
+                #activation de la somme 1>0 sinon 0
+                config[cellule] = f_activation(somme)
+                a.append(config[cellule])
+             print(" -> ", a, end="")
+        print()
